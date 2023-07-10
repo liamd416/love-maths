ad15aa73f6d63224058c4148b97db00a0fd870cd
Liam Davis <liamd416@gmail.com>
2023-07-10 12:33:59 +0000

    Add code to check user and respond to user

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", function() {
     for (let button of buttons) {
         button.addEventListener("click", function() {
             if (this.getAttribute("data-type") === "submit") {
-                alert("You clicked Submit!");
+                checkAnswer();
             } else {
                 let gameType = this.getAttribute("data-type");
                 runGame(gameType);
@@ -34,9 +34,22 @@ function runGame(gameType) {
         throw `Unknown game type: ${gameType}. Aborting!`
     }
 }
-
+/**
+ * checks the answer against the first element in
+ * the returned calculateCorrectAnswer array
+ */
 function checkAnswer() {
+    let userAnswer = parseInt(document.getElementById("answer-box").value);
+    let calculatedAnswer = calculateCorrectAnswer();
+    let isCorrect = userAnswer === calculatedAnswer[0];
 
+    if(isCorrect) {
+        alert("Hey! You got it right! :D")
+    } else {
+        alert(`You answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`)
+    }
+
+    runGame(calculatedAnswer[1]);
 }
 
 /**
